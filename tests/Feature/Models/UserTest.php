ac05267f9aab3ce70dc8e5a85d97273e1a26a494
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2023-04-02 20:14:43 +0430

    first commit

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        
+    }
+}
